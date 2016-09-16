96ab9c3d2193496ba2d1f365b786cc83666e8937
Damien McDonnell <damien@refresh.ie>
2016-09-16 14:50:07 +0100

    Added actions to the  remove list in CleanUp function

--- public/class-studio-manager-public.php
+++ public/class-studio-manager-public.php
@@ -122,9 +122,12 @@ class Studio_Manager_Public {
             remove_action( 'wp_head', 'rel_canonical', 10, 0 );
             remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
             remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 ); // Adjacent post rel link
+            remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0); // Display relational links for the posts adjacent to the current post.
             remove_action( 'wp_head', 'wp_generator' );               // WP Version
             remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+            remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
             remove_action( 'wp_print_styles', 'print_emoji_styles' );
+            remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 
         }
